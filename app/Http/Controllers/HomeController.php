abc0c5ab3028938e61368be542768e75f1cb4aa0
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-03-15 11:46:35 +0200

    solve ammed bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,14 +11,15 @@ class HomeController extends Controller
     //
     public function index()
     {
+        $categories = Category::where([['is_published', 1]])->with('Items')->orderBy('sort_order', 'asc')->get();
+
         $last_opened = session('last_opened') ?? Cookie::get('last_opened');
         $current_time = now();
         if (!$last_opened || $last_opened->diffInMinutes($current_time) >= 30) {
             session(['last_opened' => $current_time]);
             Cookie::queue(Cookie::make('last_opened', $current_time, 30));
-            return view('Pages.Home')->with('open_model', true);
+            return view('Pages.Home',compact('categories'))->with('open_model', true);
         }
-        $categories = Category::where([['is_published', 1]])->with('Items')->orderBy('sort_order', 'asc')->get();
         // dd($categories);
         return view('Pages.Home',compact('categories'))->with('open_model', false);
     }
